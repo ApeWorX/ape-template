9dbef1464c44db551fa640ca5c9b389e7bbff8f9
Doggo <27985126+sabotagebeats@users.noreply.github.com>
2022-08-02 09:14:53 -0700

    chore: update ape pin to v0.4.0 (#6)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,40 @@
+import os
+import shutil
+from pathlib import Path
+
 import pytest
 from click.testing import CliRunner
+
+from ape_template._cli import cli as template_cli
 
 
 @pytest.fixture(scope="session")
 def cli():
-    from ape_template._cli import cli
-
-    yield cli
+    return template_cli
 
 
 @pytest.fixture(scope="session")
 def runner():
     yield CliRunner()
+
+
+@pytest.fixture(scope="session", autouse=True)
+def from_tests_dir():
+    tests_dir = Path(__file__).parent
+    curr_dir = os.curdir
+    os.chdir(str(tests_dir))
+    yield tests_dir
+    os.chdir(curr_dir)
+
+
+@pytest.fixture
+def project_path():
+    project_path = Path(__file__).parent / "TokenProject"
+
+    if project_path.exists():
+        shutil.rmtree(project_path)
+
+    yield project_path
+
+    if project_path.exists():
+        shutil.rmtree(project_path)
--- tests/test_template.py
+++ tests/test_template.py
@@ -1,20 +1,10 @@
-import shutil
-from pathlib import Path
-
-
-def test_template(runner, cli):
-    project_path = Path("./TokenProject/")
-
-    if project_path.exists():
-        shutil.rmtree(project_path)
-
+def test_template(runner, cli, project_path):
     result = runner.invoke(cli, ["gh:ApeAcademy/ERC20"], input="\n\n\n\n\n\n\n\n\n\n\n")
-    assert result.exit_code == 0
+    assert result.exit_code == 0, result.output
     assert project_path.exists()
     assert project_path.joinpath("contracts").exists()
     assert project_path.joinpath("scripts").exists()
     assert project_path.joinpath("tests").exists()
-    shutil.rmtree(project_path)
 
 
 def test_help(runner, cli):
