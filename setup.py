9c00943d70d649f7f3841914321482397752f03d
NotPeopling2day <32708219+NotPeopling2day@users.noreply.github.com>
2023-11-22 17:08:05 +0100

    chore: update versions

--- setup.py
+++ setup.py
@@ -10,14 +10,15 @@ extras_require = {
         "hypothesis>=6.2.0,<7.0",  # Strategy-based fuzzer
     ],
     "lint": [
-        "black>=23.3.0,<24",  # auto-formatter and linter
-        "mypy>=0.991,<1",  # Static type analyzer
-        "types-setuptools",  # Needed due to mypy typeshed
-        "flake8>=6.0.0,<7",  # Style linter
+        "black>=23.11.0,<24",  # Auto-formatter and linter
+        "mypy>=1.7.0,<2",  # Static type analyzer
+        "types-setuptools",  # Needed for mypy type shed
+        "flake8>=6.1.0,<7",  # Style linter
         "isort>=5.10.1,<6",  # Import sorting linter
-        "mdformat>=0.7.16",  # Auto-formatter for markdown
+        "mdformat>=0.7.17",  # Auto-formatter for markdown
         "mdformat-gfm>=0.3.5",  # Needed for formatting GitHub-flavored markdown
         "mdformat-frontmatter>=0.4.1",  # Needed for frontmatters-style headers in issue templates
+        "mdformat-pyproject>=0.0.1",  # Allows configuring in pyproject.toml
     ],
     "release": [  # `release` GitHub Action job uses this
         "setuptools",  # Installation tool
@@ -59,7 +60,7 @@ setup(
     install_requires=[
         "click",  # Use same version as eth-ape
         "cookiecutter>=2.1.1,<2.2.0",
-        "eth-ape>=0.6.0,<0.7",
+        "eth-ape>=0.7.0,<0.8",
     ],
     entry_points={
         "ape_cli_subcommands": [
